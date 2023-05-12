a20feb416325e4feec8ba391a1b1ccad9392e025
Dave Marsland <86118945+finematterdave@users.noreply.github.com>
2023-05-12 21:36:22 +0100

    Update gatsby-browser.js

--- src/gatsby-browser.js
+++ src/gatsby-browser.js
@@ -1,59 +1,66 @@
 // We override loadPage & loadPagesync to fix canonical redirects
 // we also override hovering to disable hover prefetch
-exports.onClientEntry = () => {
-  const loader = window.___loader;
+export const onClientEntry = () => {
+  const loader = window.___loader
 
+  console.log(process.env.NODE_ENV)
   // if development or no loader exists we shouldn't do anything
-  if (process.env.NODE_ENV === 'development' || !loader) {
-    return;
+  if (process.env.NODE_ENV === "development" || !loader) {
+    return
   }
 
-  const pagePath = window.pagePath;
-  const location = window.location;
+  const pagePath = window.pagePath
+  const location = window.location
 
   if (
     pagePath &&
     pagePath !== location.pathname &&
-    pagePath !== location.pathname + '/'
+    pagePath !== location.pathname + "/"
   ) {
-    const originalLoadPageSync = loader.loadPageSync;
-    const originalLoadPage = loader.loadPage;
+    const originalLoadPageSync = loader.loadPageSync
+    const originalLoadPage = loader.loadPage
 
-    loader.loadPageSync = path => {
-      let pageResources;
-      // if the path is the same as our current page we know it's not a prefetch
-      if (path === location.pathname) {
-        pageResources = originalLoadPageSync(pagePath);
+    loader.loadPageSync = (path) => {
+      let pageResources
+      // With Gatsby V4, path has been appended with window.location.search when invoking loadPageSync
+      const pathWithoutQueryParams = path.replace(location.search, "")
+
+      // if the pathWithoutQueryParams is the same as our current page we know it's not a prefetch
+      if (pathWithoutQueryParams === location.pathname) {
+        pageResources = originalLoadPageSync(pagePath)
       } else {
-        pageResources = originalLoadPageSync(path);
+        pageResources = originalLoadPageSync(pathWithoutQueryParams)
       }
 
       if (pageResources.page) {
-        pageResources.page.matchPath = '*';
+        pageResources.page.matchPath = "*"
       }
 
-      return pageResources;
-    };
+      return pageResources
+    }
 
-    loader.loadPage = path => {
-      let pageResources;
-      // if the path is the same as our current page we know it's not a prefetch
-      if (path === location.pathname) {
-        pageResources = originalLoadPage(pagePath);
+    loader.loadPage = (path) => {
+      let pageResources
+      // With Gatsby V4, path has been appended with window.location.search when invoking loadPage
+      const pathWithoutQueryParams = path.replace(location.search, "")
+
+      // if the pathWithoutQueryParams is the same as our current page we know it's not a prefetch
+      if (pathWithoutQueryParams === location.pathname) {
+        pageResources = originalLoadPage(pagePath)
       } else {
-        pageResources = originalLoadPage(path);
+        pageResources = originalLoadPage(pathWithoutQueryParams)
       }
 
       if (pageResources.page) {
-        pageResources.page.matchPath = '*';
+        pageResources.page.matchPath = "*"
       }
 
-      return pageResources;
-    };
+      return pageResources
+    }
   }
 
   // disable hovering prefetching as we don't know if we can.
-  loader.hovering = () => {};
+  loader.hovering = () => {}
 };
 
 // we also need to disable prefetching as we don't know the exact page-data path.
